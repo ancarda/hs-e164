6e5e8885f144d4780208d4f621873c9d6bea11bc
Mark Dain <mark@markdain.net>
2018-09-16 00:20:50 +0100

    Break tests into smaller, logical sections

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,43 +15,36 @@ justParseE164 :: String -> E164Number
 justParseE164 s = fromJust $ parseE164 s
 
 main :: IO ()
-main = hspec $ do
-  describe "E164Number" $
+main = hspec $
+  describe "E164Number" $ do
     it "should parse E.164 numbers" $ do
-      -- TODO(ancarda): These tests can be activated if these methods become
-      -- reachable by the unit tests. This might be possible if there are
-      -- internal modules that aren't exported to the outside world, but are
-      -- visible to the tests.
-      --
-      -- I have no idea how to accomplish that.
-      --
-      -- These can't be exported as they are internal implementation details.
+      -- TODO(ancarda): These can be tested once moved to internal modules.
       {-
       onlyNumbers "+1 (202) 555-8601"   `shouldBe` "12025558601"
       filter isNumeric "1-800-555-8601" `shouldBe` "18005558601"
-      -}
 
-      isUSCA "202" `shouldBe` (Just $ Left "US")
-      isUSCA "403" `shouldBe` (Just $ Left "CA")
-
-      {-
       candidates "+447447099060" `shouldBe` Just (44, Left "GB")
       candidates "+12025558601"  `shouldBe` Just (1,  Left "US")
       candidates "+14035558601"  `shouldBe` Just (1,  Left "CA")
       -}
 
-      prefix  (justParseE164 "1-202-555-8601")  `shouldBe` 1
-      country (justParseE164 "1-202-555-8601")  `shouldBe` Just "US"
-      purpose (justParseE164 "1-202-555-8601")  `shouldBe` Nothing
+      prefix  (justParseE164 "1-202-555-8601") `shouldBe` 1
+      country (justParseE164 "1-202-555-8601") `shouldBe` Just "US"
+      purpose (justParseE164 "1-202-555-8601") `shouldBe` Nothing
 
-      show    (justParseE164 "+1.210.555.7890") `shouldBe` "+1.2105557890"
-      show    (justParseE164 "+1.800.555.7890") `shouldBe` "+1.8005557890"
-      show    (justParseE164 "+1.368.555.7890") `shouldBe` "+1.3685557890"
+    it "should support the North American Numbering Plan (+1)" $ do
+      isUSCA "202" `shouldBe` (Just $ Left "US")
+      isUSCA "403" `shouldBe` (Just $ Left "CA")
 
-      human   (justParseE164 "+1.210.555.7890") `shouldBe` "(210) 555-7890"
-      human   (justParseE164 "+1.800.555.7890") `shouldBe` "1-800-555-7890"
-      human   (justParseE164 "+1.368.555.7890") `shouldBe` "+1.3685557890"
+    it "should convert E164Number to string" $ do
+      show  (justParseE164 "+1.210.555.7890") `shouldBe` "+1.2105557890"
+      show  (justParseE164 "+1.800.555.7890") `shouldBe` "+1.8005557890"
+      show  (justParseE164 "+1.368.555.7890") `shouldBe` "+1.3685557890"
+      human (justParseE164 "+1.210.555.7890") `shouldBe` "(210) 555-7890"
+      human (justParseE164 "+1.800.555.7890") `shouldBe` "1-800-555-7890"
+      human (justParseE164 "+1.368.555.7890") `shouldBe` "+1.3685557890"
 
+    it "should do DNS mapping of E.164 numbers" $ do
       e164arpa (justParseE164 "+1.210.555.7890") `shouldBe`
         "0.9.8.7.5.5.5.0.1.2.1.e164.arpa."
       e164arpa (justParseE164 "+44.7700900461")  `shouldBe`
